af8af0029c03c5409d1313f235b892b13f28f314
Ashraf Sangi <ashraf.sangi27@gmail.com>
2024-09-12 22:16:33 -0400

    created controller

--- src/main/java/com/example/csc311_lab02/HelloController.java
+++ src/main/java/com/example/csc311_lab02/HelloController.java
@@ -1,14 +1,33 @@
 package com.example.csc311_lab02;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     @FXML
-    private Label welcomeText;
-
+    private TextField annualInterestRate;
     @FXML
-    protected void onHelloButtonClick() {
-        welcomeText.setText("Welcome to JavaFX Application!");
+    private TextField numberOfYears;
+    @FXML
+    private TextField loanAmount;
+    @FXML
+    private TextField monthlyPayment;
+    @FXML
+    private TextField totalPayment;
+    double interest;
+    int years;
+    double loan;
+    double monthly;
+    double total;
+
+    public void calculate(ActionEvent event){
+        years = Integer.parseInt(numberOfYears.getText());
+        interest = Double.parseDouble(annualInterestRate.getText());
+        loan = Double.parseDouble(loanAmount.getText());
+
     }
-}
+
+
+    }
